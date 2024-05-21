d1c48d3f0846c57de079db8c0f1fbcc73871b354
Özgür Demir <145914218+OzgurD-17@users.noreply.github.com>
2024-05-21 16:15:07 +0300

    update

--- Web Proje Deneme1/login.php
+++ Web Proje Deneme1/login.php
@@ -2,27 +2,24 @@
 $username = $_POST['username'];
 $password = $_POST['password'];
 
-if (empty($username) || empty($password)) 
-{
-    header("Location: Login.html");
+if (empty($username) || empty($password)) {
+    echo "<script type='text/javascript'>alert('Kullanıcı adı veya parola boş olamaz. Lütfen tekrar deneyin.');window.location.href='Login.html';</script>";
     exit();
 }
 
-if (!filter_var($username, FILTER_VALIDATE_EMAIL)) 
-{
-    header("Location: Login.html");
+if (!filter_var($username, FILTER_VALIDATE_EMAIL)) {
+    echo "<script>
+    alert('Geçersiz e-posta adresi. Lütfen tekrar deneyin.');
+    window.location.href='Login.html';
+    </script>";
     exit();
 }
 
 $expectedUsername = 'b221210017@sakarya.edu.tr';
 $expectedPassword = 'b221210017';
 
-if ($username === $expectedUsername && $password === $expectedPassword) 
-{
-    echo "<script>alert('Giriş başarılı, hoş geldiniz!');</script>";
-} 
-else 
-{
-    echo "<script>alert('Kullanıcı adı veya parola yanlış. Lütfen tekrar deneyin.');</script>";
+if ($username === $expectedUsername && $password === $expectedPassword) {
+    echo "<script>alert('Giriş başarılı, hoş geldiniz!');window.location.href='Login.html';</script>";
+} else {
+    echo "<script>alert('Kullanıcı adı veya parola yanlış. Lütfen tekrar deneyin.');window.location.href='Login.html';</script>";
 }
-?>
